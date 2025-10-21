b5f38738219d515f0ca69da655ea8e3e3a33a3c4
Heeme Beeme <heemebeeme@gmail.com>
2025-10-21 12:09:19 -0300

    animations and activity
    one trigger starts 2 animations (floating and falling) and the other makes the orb appear and disappear

--- Assets/Scripts/TriggersActivity.cs
+++ Assets/Scripts/TriggersActivity.cs
@@ -1,19 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class TriggersColour : MonoBehaviour
+public class TriggersActivity : MonoBehaviour
 {
+    public GameObject Orb;
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger Entered By: " + other.gameObject.name);
-    }
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Trigger Stayed By: " + other.gameObject.name);
+        Orb.SetActive(false);
     }
     void OnTriggerExit(Collider other)
     {
         Debug.Log("Trigger Exited By: " + other.gameObject.name);
+        Orb.SetActive(true);
     }
 }
--- Assets/Scripts/TriggersAnimation.cs
+++ Assets/Scripts/TriggersAnimation.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 
 public class TriggersAnimation : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Trigger Entered By: " + other.gameObject.name);
-    }
+    public Animator OrbAnimator;
     void OnTriggerStay(Collider other)
     {
         Debug.Log("Trigger Stayed By: " + other.gameObject.name);
+        OrbAnimator.Play("OrbAnim");
     }
     void OnTriggerExit(Collider other)
     {
         Debug.Log("Trigger Exited By: " + other.gameObject.name);
+        OrbAnimator.Play("OrbFallAnim");
     }
 }
